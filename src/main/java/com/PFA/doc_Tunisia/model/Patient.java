fff925bfcac79e04599e4e3052f8ddc0f2a28dc2
chouroukchakroun <chouroukchakroun.net>
2023-04-01 01:11:46 +0200

    version 1

---
+++ src/main/java/com/PFA/doc_Tunisia/model/Patient.java
@@ -0,0 +1,2 @@
+package com.PFA.doc_Tunisia.model;public class Patient {
+}
---
+++ src/main/java/com/PFA/doc_Tunisia/model/Person.java
@@ -0,0 +1,11 @@
+package com.PFA.doc_Tunisia.model;
+
+public class User {
+    private  Long id;
+
+    private String userName;
+
+    private String lastName;
+
+
+}
