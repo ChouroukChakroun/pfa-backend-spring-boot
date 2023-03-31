c44461b8d0c1d3efa642e5c3eade944a1c26cac7
chouroukchakroun <chouroukchakroun.net>
2023-03-31 23:22:38 +0200

    add attributes

--- src/main/java/com/PFA/doc_Tunisia/model/Doctor.java
+++ src/main/java/com/PFA/doc_Tunisia/model/Doctor.java
@@ -2,6 +2,14 @@ package com.PFA.doc_Tunisia.model;
 
 public class Doctor {
 
-    private long id;
+    private Long id;
+
     private String firstName;
+
+    private String lastName;
+
+    private float priceConsultation;
+
+    private String address;
+
 }
